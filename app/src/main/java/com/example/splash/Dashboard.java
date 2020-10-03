ba0dbb680a46a12016f1b473e6a1603eaeab068f
Lasindu Kushan <67940538+lasindukushan@users.noreply.github.com>
2020-10-03 11:18:47 +0530

    third Commit

--- app/src/main/java/com/example/splash/Dashboard.java
+++ app/src/main/java/com/example/splash/Dashboard.java
@@ -1,42 +1,40 @@
 package com.example.splash;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
-public class Dashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    ImageView price;
-    ImageView myImageCrop;
+import java.util.List;
+
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    ImageView menuIcon;
+
+    //drawer menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuicon;
+
     ImageView pltshedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
 
-        price = findViewById(R.id.imgPrice);
-        myImageCrop = findViewById(R.id.imgCrop);
 
-        price.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Dashboard.this , price.class);
-                startActivity(intent);
-            }
-        });
 
-        //moving to the crop details page
-        myImageCrop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Dashboard.this , CropDetails.class);
-                startActivity(intent);
-            }
-        });
         pltshedule = findViewById(R.id.imgPlantingSchedule);// get the relevant imageview id
 
         pltshedule.setOnClickListener(new View.OnClickListener() {//set on click listner for the taken id
@@ -44,8 +42,68 @@ public class Dashboard extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(Dashboard.this,Plantshedule_u.class);
                 startActivity(i);
+
             }
+
         });
 
+        menuIcon = findViewById(R.id.menuIcon);
+
+        //menu huk
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuicon = findViewById(R.id.menuIcon);
+
+        //nav drawer
+
+        navigationDrawer();
+
+
     }
-}
+
+    private void navigationDrawer() {
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else
+            super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+
+            case R.id.nav_district:
+                Intent intent = new Intent(Dashboard.this,district.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_market:
+                Intent intent1 = new Intent(Dashboard.this,price.class);
+                startActivity(intent1);
+                break;
+        }
+        return false;
+    }
+    }
