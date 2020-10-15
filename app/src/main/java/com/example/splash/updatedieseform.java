469a14d36519ec0a4fe3d637efbd7e32c834da38
yamix9 <yamithau@gmail.com>
2020-10-15 18:30:02 +0530

    button color change

--- app/src/main/java/com/example/splash/updatedieseform.java
+++ app/src/main/java/com/example/splash/updatedieseform.java
@@ -18,12 +18,12 @@ import com.google.firebase.database.ValueEventListener;
 
 public class updatedieseform extends AppCompatActivity {
 
-    EditText Name, Cause_it, Look_like, Can_do;
+    EditText Name, Cause_it, Look_like, Can_do, ID;
     Button btnupdate;
     DatabaseReference dbRef;
     DeisesID dis;
 
-    long maxid=0;
+    long maxid = 0;
 
 
     @Override
@@ -31,56 +31,57 @@ public class updatedieseform extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_updatedieseform);
 
-        Name = findViewById(R.id.editTextTextPersonName);
-        Cause_it = findViewById(R.id.inputbox2);
-        Look_like = findViewById(R.id.inputbox3);
-        Can_do = findViewById(R.id.inputbox4);
+        ID = findViewById(R.id.uinputboxid);
+        Name = findViewById(R.id.ueditTextTextPersonName);
+        Cause_it = findViewById(R.id.uinputbox2);
+        Look_like = findViewById(R.id.uinputbox3);
+        Can_do = findViewById(R.id.uinputbox4);
 
-        btnupdate = findViewById(R.id.btn_update);
+        btnupdate = findViewById(R.id.btn_u3x);
 
         dis = new DeisesID();
 
-        btnupdate.setOnClickListener(new android.view.View.OnClickListener(){
-        @Override
-        public void onClick (View view) {
+        btnupdate.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-            dbRef = FirebaseDatabase.getInstance().getReference().child("DeisesID");
-            dbRef.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.exists())
-                        maxid = (dataSnapshot.getChildrenCount());
+                dbRef = FirebaseDatabase.getInstance().getReference().child("DeisesID");
+                dbRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists())
+                            maxid = (dataSnapshot.getChildrenCount());
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                try {
+                    if (TextUtils.isEmpty(Name.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Empty Name", Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(Cause_it.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Empty Cause it", Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(Look_like.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Empty Look like", Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(Can_do.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Empty Can do", Toast.LENGTH_SHORT).show();
+                    else {
+                        dis.setName(Name.getText().toString().trim());
+                        dis.setCause_it(Cause_it.getText().toString().trim());
+                        dis.setLook_like(Look_like.getText().toString().trim());
+                        dis.setCan_do(Can_do.getText().toString().trim());
+                        //dbRef.child(String.valueOf(maxid + 1)).setValue(dis);
+                        dbRef.child(String.valueOf(ID.getText())).setValue(dis);
+                        Toast.makeText(updatedieseform.this, R.string.dataupdate, Toast.LENGTH_LONG).show();
+                    }
+                } finally {
 
                 }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
-
-            try {
-                if (TextUtils.isEmpty(Name.getText().toString()))
-                    Toast.makeText(getApplicationContext(), "Empty Name", Toast.LENGTH_SHORT).show();
-                else if (TextUtils.isEmpty(Cause_it.getText().toString()))
-                    Toast.makeText(getApplicationContext(), "Empty Cause it", Toast.LENGTH_SHORT).show();
-                else if (TextUtils.isEmpty(Look_like.getText().toString()))
-                    Toast.makeText(getApplicationContext(), "Empty Look like", Toast.LENGTH_SHORT).show();
-                else if (TextUtils.isEmpty(Can_do.getText().toString()))
-                    Toast.makeText(getApplicationContext(), "Empty Can do", Toast.LENGTH_SHORT).show();
-                else {
-                    dis.setName(Name.getText().toString().trim());
-                    dis.setCause_it(Cause_it.getText().toString().trim());
-                    dis.setLook_like(Look_like.getText().toString().trim());
-                    dis.setCan_do(Can_do.getText().toString().trim());
-                    dbRef.child(String.valueOf(maxid + 1)).setValue(dis);
-
-                    Toast.makeText(updatedieseform.this, R.string.dataupdate, Toast.LENGTH_LONG).show();
-                }
-            } finally {
-
             }
-        }
-    });
+        });
     }
 }
